0cad42ce2cf7f9dd9be6badd9f6180e4259c9d2c
noaione <noaione0809@gmail.com>
2021-05-08 15:59:53 +0700

    projectsdata: add naoTimes

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,14 @@
+const localizedProjects1 = {
+  id: 'Sebuah Bot Discord untuk melakukan tracking garapan Fansub',
+  en: 'A Discord Bot to help manage Anime Fansub projects',
+}
+
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What is you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'naoTimes',
+    description: localizedProjects1,
+    imgSrc: '/static/images/projects/naotimes.png',
+    href: 'https://naoti.me',
   },
 ]
 
