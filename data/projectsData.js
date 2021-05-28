5904a58aed788d2483d5c01231cd0d87590d4103
noaione <noaione0809@gmail.com>
2021-05-28 18:37:36 +0700

    projects: add ihaapi

--- data/projectsData.js
+++ data/projectsData.js
@@ -3,12 +3,23 @@ const localizedProjects1 = {
   en: 'A Discord Bot to help manage Anime Fansub projects',
 }
 
+const localizedProjects2 = {
+  id: 'Sebuah API pribadi dengan fokus tracking live stream VTuber',
+  en: 'A personal API proejct with focus of tracking live stream of a VTuber',
+}
+
 const projectsData = [
   {
     title: 'naoTimes',
     description: localizedProjects1,
     imgSrc: '/static/images/projects/naotimes.png',
     href: 'https://naoti.me',
+  },
+  {
+    title: 'ihateani.me API',
+    description: localizedProjects2,
+    imgSrc: '/static/images/projects/ihaapi.png',
+    href: 'https://api.ihateani.me',
   },
 ]
 
