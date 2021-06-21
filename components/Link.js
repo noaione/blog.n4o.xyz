4d7637b45c15541331c2deb4716d7cc1d8b3c5fa
noaione <noaione0809@gmail.com>
2021-06-21 18:13:50 +0700

    Link: add break-words

--- components/Link.js
+++ components/Link.js
@@ -1,23 +1,38 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import Link from 'next/link'
 
+function isS(s) {
+  return typeof s === 'string' && s.length > 0
+}
+
 const CustomLink = ({ href, locale, ...rest }) => {
   const isInternalLink = href && href.startsWith('/')
   const isAnchorLink = href && href.startsWith('#')
+  const { className, ...realRest } = rest
 
   if (isInternalLink) {
     return (
       <Link href={href} locale={locale} passHref>
-        <a {...rest} />
+        <a className={`break-words ${isS(className) ? className : ''}`} {...realRest} />
       </Link>
     )
   }
 
   if (isAnchorLink) {
-    return <a href={href} {...rest} />
+    return (
+      <a href={href} className={`break-words ${isS(className) ? className : ''}`} {...realRest} />
+    )
   }
 
-  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
+  return (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+      className={`break-words ${isS(className) ? className : ''}`}
+      {...realRest}
+    />
+  )
 }
 
 export default CustomLink
