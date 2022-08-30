846c66f3bde6a4fa202112c6ba63cfa479317d36
noaione <noaione0809@gmail.com>
2022-08-30 11:39:15 +0800

    fix: api hits route

--- pages/api/hits.ts
+++ pages/api/hits.ts
@@ -1,35 +1,58 @@
+import { isNone } from '@/lib/utils';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 interface PageViewResponse {
-  count: number;
-  name: string;
-  pageviews: number;
+  results?: {
+    visitors?: {
+      value?: number;
+    };
+  };
 }
 
-async function fetchPagesViews(): Promise<PageViewResponse[]> {
-  const response = await fetch('https://tr.n4o.xyz/api/stats/blog.n4o.xyz/pages');
+const { PLAUSIBLE_BASE_URL, PLAUSIBLE_SITE_ID, PLAUSIBLE_TOKEN } = process.env;
+
+async function fetchPagesViews(pages?: string): Promise<PageViewResponse | undefined> {
+  const url = `${PLAUSIBLE_BASE_URL}/api/v1/stats/aggregate`;
+  const urlWithParam = new URL(url);
+  urlWithParam.searchParams.append('site_id', PLAUSIBLE_SITE_ID);
+  if (!isNone(pages)) {
+    let decodedUrl = decodeURIComponent(pages);
+    if (!decodedUrl.startsWith('/')) {
+      decodedUrl = '/' + decodedUrl;
+    }
+    urlWithParam.searchParams.append('filters', `event:page==${decodedUrl}`);
+  }
+  const response = await fetch(urlWithParam.toString(), {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${PLAUSIBLE_TOKEN}`,
+    },
+  });
   if (response.status < 300) {
     return await response.json();
   }
-  return [];
+  return undefined;
+}
+
+function pickFirstOne(data?: string | string[]): string | null {
+  if (isNone(data)) {
+    return null;
+  }
+  if (Array.isArray(data)) {
+    return data[0];
+  }
+  return data;
 }
 
 export default async function ViewCountAPI(req: NextApiRequest, res: NextApiResponse) {
+  if (isNone(PLAUSIBLE_BASE_URL) || isNone(PLAUSIBLE_SITE_ID) || isNone(PLAUSIBLE_TOKEN)) {
+    res.status(500).json({ hits: 0 });
+  }
   const query = req.query || {};
-  const apiRes = await fetchPagesViews();
-  if (typeof query?.slug === 'string') {
-    const pageView = apiRes.findIndex((e) => e.name === query.slug);
-    if (pageView === -1) {
-      res.json({ hits: 0 });
-    } else {
-      const views = apiRes[pageView].count ?? apiRes[pageView].pageviews;
-      res.json({ hits: views });
-    }
+  const apiRes = await fetchPagesViews(pickFirstOne(query.slug));
+  if (isNone(apiRes)) {
+    res.json({ hits: 0 });
   } else {
-    let totalViews = 0;
-    apiRes.forEach((e) => {
-      totalViews += e.count ?? e.pageviews;
-    });
-    res.json({ hits: totalViews });
+    res.json({ hits: apiRes?.results?.visitors?.value || 0 });
   }
 }
